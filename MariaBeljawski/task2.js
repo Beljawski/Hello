f38e138a42ff561408a10cf8ba6554b5b2500b7a
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-07-31 14:10:35 +0200

    new finaly version

--- MariaBeljawski/task2.js
+++ MariaBeljawski/task2.js
@@ -1,7 +1,14 @@
 // Задача. Вывести имена и баланс не заблокированных юзеров не из China и не из France, если их баланс находится в диапазоне значений от 10 до 1000 (не вкл.)
 
-db.users.find({
-  country: { $nin: ["China", "France"] },
-  is_blocked: { $ne: true },
-  balance: { $gt: 10, $lt: 1000 },
-});
+db.users.find(
+  {
+    country: { $nin: ["China", "France"] },
+    is_blocked: { $ne: true },
+    balance: { $gt: 10, $lt: 1000 },
+  },
+  {
+    fullname: 1,
+    balance: 1,
+    _id: 0,
+  }
+);
